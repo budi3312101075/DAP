db16efeb5497f51f7c470c8f488f4ed96275fedc
budi3312101075 <budiprayoga5103@gmail.com>
2024-01-09 21:41:58 +0700

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
+import Home from "./Pages/Home";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Moleculs/FormLogin.jsx
+++ src/Components/Moleculs/FormLogin.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import Button from "../Atoms/Button";
+import axios from "axios";
 
 const FormLogin = () => {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState, setError } = useForm();
+  const navigate = useNavigate();
 
-  const onSubmit = (data) => {
-    // Lakukan sesuatu dengan data yang dikirimkan melalui formulir
-    console.log(data);
+  const onSubmit = async (data) => {
+    try {
+      await axios.post("http://localhost:5000/Login", data);
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      navigate("/Home");
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -16,7 +25,7 @@ const FormLogin = () => {
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col w-full gap-3"
       >
-        {formState.errors.Username && (
+        {formState.errors.username && (
           <span className="text-red-600">
             Hanya huruf dan angka yang diperbolehkan untuk Username
           </span>
@@ -24,20 +33,30 @@ const FormLogin = () => {
         <input
           className="rounded-md px-2 py-[1px]"
           type="text"
-          {...register("Username", {
+          {...register("username", {
             required: true,
             maxLength: 20,
             pattern: /^[A-Za-z0-9]+$/i,
           })}
           placeholder="Username"
         />
+
+        {formState.errors.password && (
+          <span className="text-red-600">Password harus diisi.</span>
+        )}
         <input
           className="rounded-md px-2 py-[1px]"
           type="password"
           {...register("password", { required: true })}
           placeholder="******"
         />
-        <Button type="submit" style="w-40 mx-auto" isi="Login" />
+
+        <Button
+          type="submit"
+          style="w-40 mx-auto"
+          isi={formState.isSubmitting ? "Logging in..." : "Login"}
+          disabled={formState.isSubmitting}
+        />
       </form>
     </div>
   );
